03132247e7f01813a20565c92e81a7e1850f5343
sinfallas <sinfallas@gmail.com>
2014-12-18 22:23:32 -0430

    * se crea el archivo cc_config.xml

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 boinc-preinstaller
 ==================
 
+![Status](https://travis-ci.org/sinfallas/boinc-preinstaller.svg) 
+
 script to install dependencies for BOINC 
--- boinc-preinstaller.sh
+++ boinc-preinstaller.sh
@@ -11,12 +11,17 @@ apt -y install libxss1 libwxgtk2.8-0 freeglut3 xscreensaver libjpeg62
 if [[ $tipo = x86_64 ]];then
 	apt -y install libstdc++5 libstdc++6 ia32-libs-i386
 fi
-ln -sv /home/$quien/Descargas/BOINC/ /var/lib/boinc/
-ln -sv /home/$quien/Descargas/BOINC/boincscr /usr/lib/xscreensaver/boincscr
+ln -sv /home/$quien/BOINC/ /var/lib/boinc/
+ln -sv /home/$quien/BOINC/boincscr /usr/lib/xscreensaver/boincscr
 ln -sv /usr/lib/i386-linux-gnu/libssl.so.1.0.0 /usr/lib/i386-linux-gnu/libssl.so.8
 ln -sv /usr/lib/i386-linux-gnu/libcrypto.so.1.0.0 /usr/lib/i386-linux-gnu/libcrypto.so.8
-# echo '- GL: /home/$quien/Descargas/BOINC/boincscr -root -boinc_dir /home/$quien/Descargas/BOINC/ \n\' >> /home/$quien/.xscreensaver
-echo "Instalacion de dependencias de BOINC completada..."
+# echo '- GL: /home/$quien/BOINC/boincscr -root -boinc_dir /home/$quien/BOINC/ \n\' >> /home/$quien/.xscreensaver
+echo "<cc_config>" > /home/$quien/BOINC/cc_config.xml
+echo "  <options>" >> /home/$quien/BOINC/cc_config.xml
+echo "    <http_1_0>1</http_1_0>" >> /home/$quien/BOINC/cc_config.xml
+echo "  </options>" >> /home/$quien/BOINC/cc_config.xml
+echo "</cc_config>" >> /home/$quien/BOINC/cc_config.xml
+echo "Descargue el instalador de BOINC y "
 rm -f /run/boinc-preinstaller.pid
 trap - INT TERM EXIT
 exit 0
