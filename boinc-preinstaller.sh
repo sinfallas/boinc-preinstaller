327344080397d6a04de5094b35a22b195dd55afe
Jose Perez <sinfallas@users.noreply.github.com>
2015-01-13 09:35:00 -0430

    * bugfix

--- boinc-preinstaller.sh
+++ boinc-preinstaller.sh
@@ -12,12 +12,12 @@ apt -y install libxss1 libwxgtk2.8-0 freeglut3 xscreensaver libjpeg62
 if [[ $tipo = x86_64 ]];then
 	apt -y install libstdc++5 libstdc++6 ia32-libs-i386
 fi
+mkdir -p /home/$quien/BOINC
 ln -sv /home/$quien/BOINC/ /var/lib/boinc/
 ln -sv /home/$quien/BOINC/boincscr /usr/lib/xscreensaver/boincscr
 ln -sv /usr/lib/i386-linux-gnu/libssl.so.1.0.0 /usr/lib/i386-linux-gnu/libssl.so.8
 ln -sv /usr/lib/i386-linux-gnu/libcrypto.so.1.0.0 /usr/lib/i386-linux-gnu/libcrypto.so.8
 # echo '- GL: /home/$quien/BOINC/boincscr -root -boinc_dir /home/$quien/BOINC/ \n\' >> /home/$quien/.xscreensaver
-mkdir -p /home/$quien/BOINC
 echo "<cc_config>" > /home/$quien/BOINC/cc_config.xml
 echo "  <options>" >> /home/$quien/BOINC/cc_config.xml
 echo "    <http_1_0>1</http_1_0>" >> /home/$quien/BOINC/cc_config.xml
